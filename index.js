b10aff292047787f3e65c02c0221f8d43aa11910
karan13kk <karan13kk@gmail.com>
2020-01-25 18:29:50 +0530

    Added server

---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+const http   = require("http");
+const dotenv = require('dotenv');
+
+
+// process errors
+process.on("uncaughtException",function(err) {
+    console.log("uncaughtException : ",err);
+    process.exit(1);
+});
+
+process.on("unhandledRejection",function(err) {
+    console.log("unhandledRejection : ",err);
+    process.exit(1);
+});
+
+// config setup
+dotenv.config();
+
+var app = require("./server/app");
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+server.listen(process.env.PORT);
+server.on("error", onError);
+server.on("listening", onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    console.log("Listening on " + bind);
+}
